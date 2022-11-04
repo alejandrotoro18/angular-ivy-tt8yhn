ede38d7f84ca72356349170ff94e57144c5708b4
alejandrotoro18 <43111688+alejandrotoro18@users.noreply.github.com>
2022-11-04 20:53:49 -0300

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SharedModule } from '../../shared/shared/shared.module';
+SharedModule
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,SharedModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
